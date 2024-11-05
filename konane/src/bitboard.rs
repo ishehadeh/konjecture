90e20554819a761e064406562cd16576c387f9ee
Ian Shehadeh <ian@shehadeh.net>
2024-11-05 09:10:27 -0500

    better debug output for boards

--- konane/src/bitboard.rs
+++ konane/src/bitboard.rs
@@ -7,7 +7,7 @@ pub enum Direction {
     Right,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 pub struct BitBoard256<const W: usize, const H: usize> {
     pub board: BitArray<4, u64>,
 }
@@ -54,5 +54,52 @@ impl<const W: usize, const H: usize> BitBoard256<W, H> {
         } else {
             self.board.clear(x + y * H)
         }
+    }
+
+    /// rotate counter clockwise
+    pub fn transpose(mut self) -> BitBoard256<H, W> {
+        assert!(W == H);
+
+        let mut block_size = W / 2;
+        while block_size > 1 {
+            let mut block_mask = BitArray::<4, u64>::new();
+
+            block_mask.set_range(block_size..block_size * 2);
+            self.board.detla_swap(block_mask, block_size * H);
+            block_size /= 2;
+        }
+
+        let mut mask0: BitArray<4, u64> = BitArray::new();
+        for i in 0..H {
+            if i % 2 == 0 {
+                mask0.set_range_step(1..W, 2);
+            }
+        }
+        self.board.detla_swap(mask0.clone(), W - 1);
+
+        if W / 2 == 1 {
+            mask0 >>= 1;
+            self.board.detla_swap(mask0, W);
+        }
+
+        BitBoard256 { board: self.board }
+    }
+}
+
+impl<const W: usize, const H: usize> std::fmt::Debug for BitBoard256<W, H> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "BitBoard256<{}, {}> {{", W, H)?;
+        for y in 0..H {
+            write!(f, "   ")?;
+            for x in 0..W {
+                if self.get(x, y) {
+                    write!(f, " 0")?;
+                } else {
+                    write!(f, " 1")?;
+                }
+            }
+            writeln!(f, "")?;
+        }
+        writeln!(f, "}}")
     }
 }
